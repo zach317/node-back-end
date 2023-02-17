4d9fd30816c80e6f53e2be405d5209697f0ae9dc
zach <zach000317@gmail.com>
2023-02-17 16:50:11 +0800

    fix: 去除无用代码

--- app.js
+++ app.js
@@ -4,15 +4,10 @@ var path = require('path')
 var cookieParser = require('cookie-parser')
 var logger = require('morgan')
 
-var indexRouter = require('./routes/index')
 var usersRouter = require('./routes/users')
 
 var app = express()
 const authentication = require('./config/authentication')
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'))
-app.set('view engine', 'ejs')
 
 app.use(logger('dev'))
 app.use(express.json())
@@ -22,7 +17,6 @@ app.use(express.static(path.join(__dirname, 'public')))
 
 app.use(authentication)
 
-app.use('/', indexRouter)
 app.use('/users', usersRouter)
 
 // catch 404 and forward to error handler
