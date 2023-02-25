3ea267db49cc707754d41b054098575d62a8405b
zach <zach000317@gmail.com>
2023-02-25 21:34:55 +0800

    Revert "fix: 去除无用代码"
    This reverts commit 4d9fd30816c80e6f53e2be405d5209697f0ae9dc.

---
+++ routes/index.js
@@ -0,0 +1,12 @@
+var express = require('express')
+const sqlQuery = require('../config/db.config')
+var router = express.Router()
+
+/* GET home page. */
+router.get('/', async function (req, res, next) {
+  const result = await sqlQuery('select * from students')
+  console.log('🚀  result', result[0])
+  res.render('index', { title: 'Express' })
+})
+
+module.exports = router
